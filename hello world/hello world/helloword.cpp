c52a65622e3bf2becee06b1a9fb222c07cfc2a6c
evanhe1 <72584623+evanhe1@users.noreply.github.com>
2020-10-08 22:00:53 -0400

    initial commit

---
+++ hello world/hello world/helloword.cpp
@@ -0,0 +1,14 @@
+//
+//  helloworld.cpp
+//  hello world
+//
+//  Created by Evan He on 10/6/20.
+//  Copyright © 2020 Evan He. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hey, this really works!" << endl;
+}
